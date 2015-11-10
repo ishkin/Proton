c701ad73d69a5852cda21074453c645dedcf9563
ishkin <inna@il.ibm.com>
2015-11-10 11:21:19 +0200

    update to work with specified delimiter

--- PETITE-Proton EvenT Injection & Time comprEssion/launchConversion.bat
+++ PETITE-Proton EvenT Injection & Time comprEssion/launchConversion.bat
@@ -1,1 +1,1 @@
-java -jar TimeSimulator.jar "dataRushHourTruncated.csv" "CNRSIntegration.json" 60000  "UpdateddataRushHourTruncated.csv" "CNRSUpdated.json"   0  1 "yyyy-MM-dd,HH:mm:ss"
+java -jar TimeSimulator.jar "dataRushHourTruncated.csv" "CNRSIntegration.json" 60000  "UpdateddataRushHourTruncated.csv" "CNRSUpdated.json"   "," 0  1 "yyyy-MM-dd,HH:mm:ss"
