df55e5b0623b0a7e612e92ddea8e23d2c5cd4fec
Uri Shani <shani@il.ibm.com>
2016-05-04 15:24:14 +0300

    Update download.pp
    fixing problems with puppet

--- puppet/modules/cep/manifests/download.pp
+++ puppet/modules/cep/manifests/download.pp
@@ -1,16 +1,48 @@
 class cep::download {
-	#We are running without a master, we will locate and run the download_artifacts.ksh locally
-	each(split($settings::modulepath, ':')) |$modules_folder| {
+	#We are running without a master, we will execute the download_artifacts.ksh locally to download the required artifacts.
 
-		exec {"exists $modules_folder/$module_name":
-			 command => '/bin/true',
-			 onlyif => "/usr/bin/test -e $modules_folder/$module_name",
-		}
+	#Define temporary storage location:
+	$local_temp_folder = "/tmp/puppet_module_$module_name.tmp"
 
-		exec { "$modules_folder/$module_name/download_artifacts.ksh":
-			command => "$modules_folder/$module_name/files/download_artifacts.ksh",
-			onlyif => "/usr/bin/test -e $modules_folder/$module_name",
-			require => [Package['ksh'],Package['xmlstarlet'],Package['zip'],Package['unzip'],Package['curl'],Exec["exists $modules_folder/$module_name"]],
-		}
+
+	#Create the temporary folder:
+	file { "$local_temp_folder":
+		ensure  => directory,
+		owner  => 'root',
+		group  => 'root',
+		mode   => '750',
 	}
+
+
+	#copy the download script the temporary folder:
+	file { "$local_temp_folder/download_artifacts.ksh":
+		ensure  => file,
+		source  => "puppet:///modules/$module_name/download_artifacts.ksh",
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0750,
+		replace => true,
+		require => File["$local_temp_folder"],
+	}
+
+
+	#Execute the download artifacts script only once, e.g: check for the ".executed-flag" file:
+	exec { "$local_temp_folder/download_artifacts.ksh":
+		command => "$local_temp_folder/download_artifacts.ksh",
+		creates => "$local_temp_folder/download_artifacts.executed-flag",
+		require => [Package['ksh'],Package['xmlstarlet'],Package['zip'],Package['unzip'],Package['curl'],File["$local_temp_folder/download_artifacts.ksh"]],
+	}
+
+	#We will only download the artifacts once, when executed successfully creates the flag file:
+	file { "$local_temp_folder/download_artifacts.executed-flag":
+		ensure  => file,
+		content => '',
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0440,
+		replace => false,
+		require => [File["$local_temp_folder"],Exec["$local_temp_folder/download_artifacts.ksh"]],
+	}
+	
 }
+
